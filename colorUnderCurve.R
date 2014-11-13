5e8f8c86efaa4e344f71444ccc6797f8df6bc72c
Arthur Lui <luiarthur@yahoo.com>
2014-11-13 12:48:54 -0700

    Update colorUnderCurve.R

--- colorUnderCurve.R
+++ colorUnderCurve.R
@@ -12,22 +12,22 @@ my.color <- function(dat,from,to) {
 color.den <- function(den,from,to) {
   # Colors area under a density within an interval
   # den has to be a density object
-  polygon(c(from, den$x[den$x> from & den$x < to], to),
-          c(0, den$y[den$x>=from & den$x <= to], 0),col="red")
+  polygon(c(from, den$x[den$x=>from & den$x<=to], to),
+          c(0, den$y[den$x>=from & den$x<=to], 0),col="red")
 }
 
 color.fn <- function(f,from,to) {
   x <- seq(from,to,by=(to-from)/10000)
-  polygon(c(from, x,    to),
-          c(0, f(x), 0),col="red")
+  polygon(c(from,x,to),
+          c(0,f(x),0),col="red")
 }
 
 
 color.emp <- function(M,from,to) {
   x <- M[,1]
   y <- M[,2]
-  polygon(c(from, x[x> from & x < to],    to),
-          c(0, y[x> from & x < to], 0),col="red")
+  polygon(c(from,x[x=>from & x<= to],to),
+          c(0,y[x>=from & x<=to],0),col="red")
 }
 
 
