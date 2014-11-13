4e8ff2aa37e16a058d9572265a85e757a64b6370
luiarthur <luiarthur@yahoo.com>
2014-11-13 12:46:32 -0700

    yes

---
+++ colorUnderCurve.R
@@ -0,0 +1,53 @@
+my.color <- function(dat,from,to) {
+  if (is(dat)[1] == "function") {
+    color.fn(dat,from,to)
+  } else if (is(dat)[1] == "density") {
+    color.den(dat,from,to)
+  } else if (is(dat)[1] == "matrix") {
+    color.emp(dat,from,to)
+  }
+}
+
+
+color.den <- function(den,from,to) {
+  # Colors area under a density within an interval
+  # den has to be a density object
+  polygon(c(from, den$x[den$x> from & den$x < to], to),
+          c(0, den$y[den$x>=from & den$x <= to], 0),col="red")
+}
+
+color.fn <- function(f,from,to) {
+  x <- seq(from,to,by=(to-from)/10000)
+  polygon(c(from, x,    to),
+          c(0, f(x), 0),col="red")
+}
+
+
+color.emp <- function(M,from,to) {
+  x <- M[,1]
+  y <- M[,2]
+  polygon(c(from, x[x> from & x < to],    to),
+          c(0, y[x> from & x < to], 0),col="red")
+}
+
+
+#Examples: ######################################################
+
+## color.den
+#  x <- rnorm(10000)
+#  denx <- density(x)
+#  plot(denx)
+#  color.den(denx,-2,5)
+#
+## color.fn
+#  fn <- function(x) dnorm(x)
+#  curve(dnorm(x),-5,5)
+#  color.fn(fn,-2,5)
+#
+## color.emp
+#  x <- seq(-5,5,length=1000)
+#  y <- dnorm(x)
+#  plot(x,y,type='l')
+#  color.emp(cbind(x,y),-2,5)
+
+
---
+++ unique.matrix.R
@@ -0,0 +1,44 @@
+unique.matrix <- function(MM) {
+  # Counts the number of unique matrices in a list.
+  # MM = a list of matrices. We want the output to be:
+  # 1) a list of UNIQUE matrices
+  # 2) a vector of their counts
+
+  eq.matrix <- function(A,B) {
+    matrices.are.identical <- F
+    if (all(dim(A) == dim(B))) {
+
+      if((A == B) || dim(A)[2] == 0) matrices.are.identical <- T
+    }
+    matrices.are.identical
+  }
+  
+  N <- length(MM)
+  vec <- 1:N
+  uniq.M <- list()
+  
+  counts <- c()
+  taken <- rep(F,N)
+
+  for (i in 1:N) {
+    if (!taken[i]) {
+      for (j in i:N) {
+        if (!taken[j]) {
+          if (eq.matrix(MM[[i]],MM[[j]])) {
+            if (i==j) {
+              counts <- c(counts,1)
+              uniq.M[[length(uniq.M)+1]] <- ZZ[[j]]
+            } else {
+              counts[length(counts)] <- counts[length(counts)] + 1         
+              taken[j] <- T
+            }
+          }
+        }
+      }
+      taken[i] <- T
+    }
+  }
+  list(counts,uniq.M)
+}
+
+
