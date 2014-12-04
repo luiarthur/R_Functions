dc7931c7313c36b776dc83319c58a6b56fd099b5
luiarthur <luiarthur@yahoo.com>
2014-12-04 12:54:26 -0700

    yes

--- colorUnderCurve.R
+++ colorUnderCurve.R
@@ -13,12 +13,13 @@ color.den <- function(den,from,to,col.den="black",col.area="red",add=F,...) {
   # Colors area under a density within an interval
   # den has to be a density object
   if (add) {
-    lines(den,col=col.den,...)
+    #lines(den,col=col.den,...)
   } else {
     plot(den,col=col.den,...)
   }
   polygon(c(from, den$x[den$x>=from & den$x<=to], to),
-          c(0, den$y[den$x>=from & den$x<=to], 0),col=col.area)
+          c(0, den$y[den$x>=from & den$x<=to], 0),
+          col=col.area,border=col.den)
 }
 
 color.fn <- function(f,from,to) {
--- plotPost.R
+++ plotPost.R
@@ -1,4 +1,47 @@
-plot.post <- function(x,main=NULL) {
+source("colorUnderCurve.R")
+bound <- function(x, dens, return.x=TRUE){
+  # Mickey Warner: 
+  # https://github.com/mickwar/r-sandbox/blob/master/mcmc/bayes_functions.R
+  # returns the x-value in dens that is closest
+  # to the given x
+  if (return.x)
+      return(dens$x[which.min(abs(dens$x-x))])
+
+  # returns the y-value in dens at the closest x
+  return(dens$y[which.min(abs(dens$x-x))])
+}
+
+col.mult = function(col1 = 0x000000, col2 = "gray50"){
+  # Mickey Warner: 
+  # https://github.com/mickwar/r-sandbox/blob/master/mcmc/bayes_functions.R
+  # returns the x-value in dens that is closest
+  # to the given x
+  if (is.character(col1))
+      val1 = t(col2rgb(col1) / 255)
+  if (is.numeric(col1))
+      val1 = t(int2rgb(col1) / 255)
+  if (is.character(col2))
+      val2 = t(col2rgb(col2) / 255)
+  if (is.numeric(col2))
+      val2 = t(int2rgb(col2) / 255)
+  rgb(val1 * val2)
+}
+
+int2rgb = function(x){
+# int2rgb()
+# convert an integer between 0 and 16777215 = 256^3 - 1,
+# or between 0 and 0xFFFFFF
+# this function is depended upon by col.mult
+  # Mickey Warner: 
+  # https://github.com/mickwar/r-sandbox/blob/master/mcmc/bayes_functions.R
+  # returns the x-value in dens that is closest
+  # to the given x
+  hex = as.character(as.hexmode(x))
+  hex = paste0("#", paste0(rep("0", 6-nchar(hex)), collapse=""), hex)
+  col2rgb(hex)
+}
+
+plot.post <- function(x,main=NULL,hpd=T,color="cornflowerblue") {
   mn.x <- round(mean(x),5)
   v.x <- round(var(x),3)
   den <- density(x)
@@ -7,11 +50,10 @@ plot.post <- function(x,main=NULL) {
   diff <- rng[2]-rng[1]
   main <- ifelse(is.null(main),"Posterior Distribution",
                          paste("Posterior Distribution for",main))
-  plot(density(x),col="blue",ylim=c(rng[1],rng[2]+diff*.3),lwd=3,
+  plot(density(x),col=color,ylim=c(rng[1],rng[2]+diff*.3),lwd=3,
        main=main)
   legend("topleft",legend=c(paste("Mean =",mn.x),
                             paste("Variance = ",v.x)),bty="n")
-  abline(v=mn.x,col="red",lwd=2)
   rng.x <- range(den$x)
   x.diff <- rng.x[2] - rng.x[1]
 
@@ -25,8 +67,26 @@ plot.post <- function(x,main=NULL) {
     axis(1,cex.axis=.5)
     axis(2,cex.axis=.5)
   par(opts)
+
+  color.den(den,rng.x[1],rng.x[2],col.den=color,col.area=color,add=T)
+  if (hpd) {
+    hpd <- get.hpd(x)
+    color.den(den,hpd[1],hpd[2],col.den=col.mult(color),
+              col.area=col.mult(color),add=T)
+  }
+
+  lines(c(mn.x,mn.x),c(0,bound(mn.x,den,ret=F)),lwd=2,col="red")
+  #abline(v=mn.x,col="red",lwd=2)
 }
 
+get.hpd <- function(x,a=.05,len=1e3) {
+  V <- matrix(seq(0,a,length=len))
+  quants <- t(apply(V,1,function(v) quantile(x,c(v,v+1-a))))
+  diff <- quants[,2]-quants[,1]
+  min.d <- V[which.min(diff)]
+  hpd <- quantile(x,c(min.d,min.d+1-a))
+  hpd
+}
 
-y <- rgamma(10000,50,.01)
-plot.post(y,"y")
+y <- rgamma(10000,5,1)
+plot.post(y,"y",color="yellow")
