0a3c2a9bb13cbc2285e461a085e2605baab43f9a
Arthur Lui <luiarthur@yahoo.com>
2015-01-16 09:41:24 -0700

    Update unique.matrix.R

--- unique.matrix.R
+++ unique.matrix.R
@@ -4,18 +4,48 @@ unique.matrix <- function(X) {
   # 1) a list of UNIQUE matrices
   # 2) a vector of their counts
 
-  lx <- length(X)
-  UX <- unique(X) 
-  lux <- length(UX)
-  counts <- rep(0,lux)
-  print(counts)
-  for (i in 1:lux) {
-    for (j in 1:lx) {
-      if (identical(UX[[i]],X[[j]])) counts[i] <- counts[i] + 1
-    }
-  }
+  #lx <- length(X)
+  #UX <- unique(X) 
+  #lux <- length(UX)
+  #counts <- rep(0,lux)
+  #for (i in 1:lux) {
+  #  for (j in 1:lx) {
+  #    if (identical(UX[[i]],X[[j]])) counts[i] <- counts[i] + 1
+  #    cat("\r",round((lx*(i-1)+j)/(lx*lux),4))
+  #  }
+  #}
 
+  #ind <- sort(counts,index.return=T,decr=T)
+  #ind <- ind$ix
+  #list(counts[ind],UX[ind])
+     
+  N <- length(MM)   
+  vec <- 1:N    
+  uniq.M <- list()    
+      
+  counts <- c()   
+  taken <- rep(F,N)   
+     
+  for (i in 1:N) {    
+    if (!taken[i]) {    
+      for (j in i:N) {    
+        if (!taken[j]) {    
+          if (identical(MM[[i]],MM[[j]])) {   
+            if (i==j) {   
+              counts <- c(counts,1)   
+              uniq.M[[length(uniq.M)+1]] <- MM[[j]]   
+            } else {    
+              counts[length(counts)] <- counts[length(counts)] + 1          
+             taken[j] <- T    
+            }   
+          }   
+        }   
+      }   
+      taken[i] <- T   
+    }   
+  }   
+      
   ind <- sort(counts,index.return=T,decr=T)
   ind <- ind$ix
-  list(counts[ind],UX[ind])
+  list(counts[ind],uniq.M[ind])
 }
