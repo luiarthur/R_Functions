ada4e400f23f71977ff6f6c273a3c4a4c7145b08
Arthur Lui <luiarthur@yahoo.com>
2015-01-16 09:14:52 -0700

    Update unique.matrix.R

--- unique.matrix.R
+++ unique.matrix.R
@@ -1,47 +1,21 @@
-unique.matrix <- function(MM) {
+unique.matrix <- function(X) {
   # Counts the number of unique matrices in a list.
-  # MM = a list of matrices. We want the output to be:
+  # X = a list of matrices. We want the output to be:
   # 1) a list of UNIQUE matrices
   # 2) a vector of their counts
 
-  eq.matrix <- function(A,B) {
-    matrices.are.identical <- F
-    if (all(dim(A) == dim(B))) {
-
-      if((A == B) || dim(A)[2] == 0) matrices.are.identical <- T
-    }
-    matrices.are.identical
-  }
-  
-  N <- length(MM)
-  vec <- 1:N
-  uniq.M <- list()
-  
-  counts <- c()
-  taken <- rep(F,N)
-
-  for (i in 1:N) {
-    if (!taken[i]) {
-      for (j in i:N) {
-        if (!taken[j]) {
-          if (eq.matrix(MM[[i]],MM[[j]])) {
-            if (i==j) {
-              counts <- c(counts,1)
-              uniq.M[[length(uniq.M)+1]] <- ZZ[[j]]
-            } else {
-              counts[length(counts)] <- counts[length(counts)] + 1         
-              taken[j] <- T
-            }
-          }
-        }
-      }
-      taken[i] <- T
+  lx <- length(X)
+  UX <- unique(X) 
+  lux <- length(UX)
+  counts <- rep(0,lux)
+  print(counts)
+  for (i in 1:lux) {
+    for (j in 1:lx) {
+      if (identical(UX[[i]],X[[j]])) counts[i] <- counts[i] + 1
     }
   }
-  
+
   ind <- sort(counts,index.return=T,decr=T)
   ind <- ind$ix
-  list(counts[ind],uniq.M[ind])
+  list(counts[ind],UX[ind])
 }
-
-
