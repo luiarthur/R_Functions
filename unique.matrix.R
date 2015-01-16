4092060fb6bd3e7a05cf814d596e27b306751220
Arthur Lui <luiarthur@yahoo.com>
2015-01-16 11:53:59 -0700

    Update unique.matrix.R

--- unique.matrix.R
+++ unique.matrix.R
@@ -1,50 +1,33 @@
-unique.matrix <- function(MM) {
+unique.matrix <- function(X) {
   # Counts the number of unique matrices in a list.
   # X = a list of matrices. We want the output to be:
   # 1) a list of UNIQUE matrices
   # 2) a vector of their counts
+  toMat <- function(s) {
+    dims <- regexpr(": \\d* \\d*",s)
+    begin <- as.integer(dims)+2
+    end <- begin+attr(dims,"match.length")
+    dims <- substr(s,begin,end)
+    pos <- as.integer(regexpr(" ",dims))
+    dims <- c(substr(dims,1,pos-1),substr(dims,pos+1,nchar(dims)))
+    dims <- as.integer(dims)
 
-  #lx <- length(X)
-  #UX <- unique(X) 
-  #lux <- length(UX)
-  #counts <- rep(0,lux)
-  #for (i in 1:lux) {
-  #  for (j in 1:lx) {
-  #    if (identical(UX[[i]],X[[j]])) counts[i] <- counts[i] + 1
-  #    cat("\r",round((lx*(i-1)+j)/(lx*lux),4))
-  #  }
-  #}
+    mat <- substr(s,1,begin-3)
+    M <- matrix(0,dims[1],dims[2])
+    if (mat>" ") {
+      vec <- as.integer(strsplit(mat,",")[[1]])
+      M <- matrix(vec,dims[1],dims[2])
+    }
 
-  #ind <- sort(counts,index.return=T,decr=T)
-  #ind <- ind$ix
-  #list(counts[ind],UX[ind])
-     
-  N <- length(MM)   
-  vec <- 1:N    
-  uniq.M <- list()    
-      
-  counts <- c()   
-  taken <- rep(F,N)   
-     
-  for (i in 1:N) {    
-    if (!taken[i]) {    
-      for (j in i:N) {    
-        if (!taken[j]) {    
-          if (identical(MM[[i]],MM[[j]])) {   
-            if (i==j) {   
-              counts <- c(counts,1)   
-              uniq.M[[length(uniq.M)+1]] <- MM[[j]]   
-            } else {    
-              counts[length(counts)] <- counts[length(counts)] + 1          
-             taken[j] <- T    
-            }   
-          }   
-        }   
-      }   
-      taken[i] <- T   
-    }   
-  }   
-      
+    M
+  }
+
+  S <- lapply(X,function(x) paste(toString(x),":",nrow(x),ncol(x),collapse=","))
+  tab <- table(unlist(S))
+  counts <- as.integer(tab)
+  mat <- names(tab)
+  uniq.M <- lapply(as.list(mat),toMat)
+  
   ind <- sort(counts,index.return=T,decr=T)
   ind <- ind$ix
   list(counts[ind],uniq.M[ind])
