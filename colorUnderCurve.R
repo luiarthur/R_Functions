02ae02b4d22700c9389a1de7ccc0d9e15fc89838
luiarthur <luiarthur@gmail.com>
2015-09-26 21:52:39 -0700

    yes

--- colorUnderCurve.R
+++ colorUnderCurve.R
@@ -23,7 +23,7 @@ color.den <- function(den,from,to,col.den="black",col.area="red",add=F,...) {
 }
 
 color.fn <- function(f,from,to,col.area="red") {
-  x <- seq(from,to,by=(to-from)/10000)
+  x <- seq(from,to,by=(to-from)/1e6)
   polygon(c(from,x,to),
           c(0,f(x),0),col=col.area,border=F)
 }
