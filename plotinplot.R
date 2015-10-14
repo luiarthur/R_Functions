057ed114487db72461999f03148d374ec970a399
luiarthur <luiarthur@gmail.com>
2015-10-13 19:53:41 -0700

    yes

--- plotinplot.R
+++ plotinplot.R
@@ -1,4 +1,5 @@
-plot.in.plot <- function(minor.plot,coords="topright",scale=1/3) {
+plot.in.plot <- function(minor.plot,coords="topright",scale=1/3,stay=F) {
+  # stay: acts like 'add' in the 'plot' function. The default is FALSE.
   # coords = x1,y1,x2,y2
   # minor.plot is a function with no parameters that plots the smaller plot
   mar <- x1 <- x2 <- y1 <- y2 <- NULL
@@ -31,6 +32,9 @@ plot.in.plot <- function(minor.plot,coords="topright",scale=1/3) {
     y2 <- y1 + (s[4]-s[3]) * (scale)
     mar <- c(1,1,1,.1)
   }
+
+  mfg <- par()$mfg # NEW
+
   opts <- par(no.readonly=T)
     par(fig = c(grconvertX(c(x1,x2),from="user",to="ndc"),
                 grconvertY(c(y1,y2),from="user",to="ndc")),
@@ -39,6 +43,28 @@ plot.in.plot <- function(minor.plot,coords="topright",scale=1/3) {
     #axis(1,cex.axis=.5)
     #axis(2,cex.axis=.5)
   par(opts)
+
+  # below here, all new
+  if (!(stay)) {
+    row.num <- mfg[1]
+    col.num <- mfg[2]
+    last.row <- mfg[3]
+    last.col <- mfg[4]
+
+    if (col.num < last.col) {
+      mfg[2] <- mfg[2] + 1
+    } else {
+      if (row.num < last.row) {
+        mfg[1] <- mfg[1] + 1
+      } else {
+        mfg[1] <- 1
+      }
+      mfg[2] <- 1
+    }
+  }
+
+  par(mfg=mfg)
+
 }
 
 #x <- rnorm(10000)
